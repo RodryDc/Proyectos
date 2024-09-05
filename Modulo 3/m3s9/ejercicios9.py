472861fc6b621f5a625d3c9fa599d78e254521ac
RodryDc <rygo.dc@gmail.com>
2024-09-05 21:49:59 +0200

    Update ejercicios9.py

--- Modulo 3/m3s9/ejercicios9.py
+++ Modulo 3/m3s9/ejercicios9.py
@@ -10,5 +10,4 @@ def area_rectangulo(base, altura):
 base = int(input("Ingresa la base: "))
 altura = int(input("Ingresa la altura: "))
 
-
 print("El área del rectángulo es: ", area_rectangulo(base, altura))
